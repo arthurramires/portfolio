d5a06c1fdedc6e1ada024915faadf6653d7835e6
arthurramires <arthurramires1802@outlook.com>
2024-07-03 18:51:49 -0400

    Adding build

---
+++ out/_next/static/UHoysjjZIkYbfA4CMEOPq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d48e6582e2fdf26c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
