fb148b56f21853a675721fa10eb151b10a039805
arthurramires <arthurramires1802@outlook.com>
2024-07-04 13:40:27 -0400

    Update deploy config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/portfolio",
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+const isGithubActions = true
+const repository       = 'portfolio'
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = repository.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'https://arthurramires-474938407.imgix.net',
+  },
+}
