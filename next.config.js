67825ea0eac76408ccccb1a604c2d4e91daee624
arthurramires <arthurramires1802@outlook.com>
2024-07-04 14:23:50 -0400

    Update deploy config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  assetPrefix: '/porfolio/',
   basePath: '/porfolio',
-  output: "export",
-  images: {
-    loader: 'imgix',
-    path: 'arthurramires-474938407.imgix.net/',
-  },
+  output: "export"
 }
