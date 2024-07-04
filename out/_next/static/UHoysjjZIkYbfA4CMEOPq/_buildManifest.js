cc554fc1329e0a07aafb8e3b3fabc1f0452775bc
arthurramires <arthurramires1802@outlook.com>
2024-07-04 13:21:28 -0400

    Removing files

--- out/_next/static/UHoysjjZIkYbfA4CMEOPq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d48e6582e2fdf26c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
